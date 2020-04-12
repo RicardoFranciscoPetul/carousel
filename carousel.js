f0843d6c6b0b5bf2de71e32d63b5bed664eaab99
Ricardo Francisco <ricardofco.35@gmail.com>
2020-04-12 12:44:24 -0500

    Carrosel con images y con doble transform :sparkles: * Se añadieron los elementos para los autores * Animacion hacia arriba en el elemento activo * Texto de autor cortado con css

--- carousel.js
+++ carousel.js
@@ -1,36 +1,45 @@
-const testimonialList = document.querySelectorAll('.single__testimonial');
-const forward = document.querySelector('#forward');
-const back = document.querySelector('#back');
+const testimonialList = document.querySelectorAll(".single-testimonial");
+const forward = document.querySelector("#forward");
+const back = document.querySelector("#back");
 let counter = 1;
 
-forward.addEventListener('click', () => moveForward());
-back.addEventListener('click', () => moveBack());
-testimonialList[1].classList.add('active');
+forward.addEventListener("click", () => moveForward());
+back.addEventListener("click", () => moveBack());
+testimonialList[1].classList.add("active");
 
 moveForward = () => {
   if (counter <= testimonialList.length - 2) {
-    testimonialList.forEach(testimonial => {
+    testimonialList.forEach((testimonial) => {
       testimonial.style.transform = `translateX(-${counter}00%)`;
-      testimonial.classList.remove('active');
+      testimonial.classList.remove("active");
+      console.log("adelante: " + counter);
     });
     counter++;
-    testimonialList[counter].classList.add('active');
+    testimonialList[counter].style.transform = `translate(-${
+      counter - 1
+    }00%,-50%)`;
   }
 };
+
 moveBack = () => {
   if (counter === 1) {
-    testimonialList.forEach(testimonial => {
-      testimonial.style.transform = `translateX(100%)`;
-      testimonial.classList.remove('active');
-      testimonialList[counter].classList.add('active');
+    testimonialList.forEach((testimonial) => {
+      testimonial.style.transform = `translate(100%, 0%)`;
+      testimonial.classList.remove("active");
+      testimonialList[counter].style.transform = `translate(100%, 0%)`;
+      
     });
+    testimonialList[0].style.transform=`translate(100%, -50%)`
   }
   if (counter !== 0) {
-    testimonialList.forEach(testimonial => {
+    testimonialList.forEach((testimonial) => {
       testimonial.style.transform = `translateX(-${counter - 2}00%)`;
-      testimonial.classList.remove('active');
+      testimonial.classList.remove("active");
+      console.log("atras: " + counter);
     });
     counter--;
-    testimonialList[counter].classList.add('active');
+    testimonialList[counter].style.transform = `translate(-${
+      counter - 1
+    }00%,-50%)`;
   }
 };
